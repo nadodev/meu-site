6c497ebb5a4d9486c80e6be80699e10990d03de6
leonardo geja <bemmenos@gmail.com>
2022-01-20 18:03:58 -0300

    adicionado title

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -19,6 +19,8 @@ const Hero = () => {
               </p>
               <a
                 href="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/7e3c89eb-06d8-4ec7-b96d-176cd9c4490b/novoCurriculo.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220120%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220120T210115Z&X-Amz-Expires=86400&X-Amz-Signature=1b56e2a17b1971eb2911cc1dbe38aef1410a96cd3513ad103d88afb8afa752b9&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Curriculum.pdf%22&x-id=GetObject"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="btn"
               >
                 Baixar CV
