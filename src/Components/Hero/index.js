42b0e9fc0b8224c830b17edf254e5cbf037da28d
leonardo geja <bemmenos@gmail.com>
2022-01-20 18:01:27 -0300

    adicionado title

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -18,7 +18,7 @@ const Hero = () => {
                 velit animi quisquam.
               </p>
               <a
-                href="https://www.notion.so/Curriculum-e8c53b6b9b404bdcb6cd95ee70d05b46#d04a8effe6b043cbb9316000b72a0a14"
+                href="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/7e3c89eb-06d8-4ec7-b96d-176cd9c4490b/novoCurriculo.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220120%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220120T210115Z&X-Amz-Expires=86400&X-Amz-Signature=1b56e2a17b1971eb2911cc1dbe38aef1410a96cd3513ad103d88afb8afa752b9&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Curriculum.pdf%22&x-id=GetObject"
                 className="btn"
               >
                 Baixar CV
